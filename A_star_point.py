088a2942864b3b39b29e47887073ccca30a62117
Brian Bock <bbock@terpmail.umd.edu>
2020-03-14 18:37:03 -0400

    added timer

--- A_star_point.py
+++ A_star_point.py
@@ -1,15 +1,23 @@
+# Import Python functions
 import cv2 
+import numpy as np
+import time
+from datetime import datetime as dtime
+# Import our functions
 from maze import Maze
 from robot import PointRobot
 import matplotlib.pyplot as plt
 from matplotlib.collections import PatchCollection
-import numpy as np
+
 
 write_to_video = False
 show_visualization = False
 userInput = False
 search_type = 'A' # D for Dijkstra, B for BFS, A for A*
 stepsize = 10 #controls the number of nodes shown in each frame of visualization
+
+# Record the start time so we can compute run time at the end
+starttime = dtime.now()
 
 # Construct maze object
 maze = Maze('maze2.txt')
@@ -38,4 +46,7 @@ else:
 
 # Visualize the path
 robot.visualize(show_visualization,write_to_video,stepsize)
+endtime = dtime.now()
+runtime=endtime-starttime
+print("Finished in "+str(warptime)+" (hours:min:sec)")
 plt.show()
